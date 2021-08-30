7c4fe4b4b9e9d3e717112d506d6c0b556c133c00
some random idiot <sittipat2554aa@hotmail.com>
2021-08-30 15:07:14 +0700

    Created a superuser and added Question and Choice models to the database.

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    text = models.CharField(max_length=500)
+    start_date = models.DateTimeField('starting date')
+    end_date = models.DateTimeField('ending date')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)
+    votes = models.IntegerField(default=0)
