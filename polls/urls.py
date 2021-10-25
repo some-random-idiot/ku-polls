034d284c626bb8be609b481d57e911b5833cc183
some random idiot <sittipat2554aa@hotmail.com>
2021-10-25 15:28:25 +0700

    Restricted users who are not logged in from voting.

--- polls/urls.py
+++ polls/urls.py
@@ -2,12 +2,13 @@
 
 from django.urls import path
 from . import views
+from django.contrib.auth.decorators import login_required
 
 app_name = 'polls'  # Namespacing the urls
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:error_message>', views.index, name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/', login_required(views.DetailView.as_view()), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote')
 ]
