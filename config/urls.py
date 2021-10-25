61854ef8c28450ca4cff42e37ecd221e8a4ca555
some random idiot <sittipat2554aa@hotmail.com>
2021-10-25 13:27:25 +0700

    Added Django auth urls to url.py.

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', RedirectView.as_view(url='polls/', permanent=True)),
     path('polls/', include('polls.urls'))
 ]
